dd39996b7e219fc9fcc75c638aeb9cb3e4db8d16
1337phtm <maeldestroyer@gmail.com>
2026-01-02 16:25:44 +0100

    aze

--- tools/push.ps1
+++ tools/push.ps1
@@ -1,4 +1,13 @@
-﻿code par ex pour push tout mon git vers github
-ou script d installation local
+﻿cd ..
+git add .
+$message = Read-Host "Enter your commit message "
+git commit -m $message
+git push origin main
 
-Tools c est pour des scripts pour lesz devs limite faire un .gitignore
+
+
+
+#code par ex pour push tout mon git vers github
+#ou script d installation local
+#
+#Tools c est pour des scripts pour lesz devs limite faire un .gitignore
