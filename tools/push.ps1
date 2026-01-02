6733ead1e1a2a933fbe0c5aeb407d1841a6f0c0e
1337phtm <maeldestroyer@gmail.com>
2026-01-02 16:30:18 +0100

    [2026-01-02 16:30:18] push by 1337phtm

--- tools/push.ps1
+++ tools/push.ps1
@@ -4,6 +4,7 @@ $timestamp = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
 $message = "[$timestamp] push by $($env:USERNAME)"
 git commit -m $message
 git push origin main
+exit
 
 #ou script d installation local
 #
